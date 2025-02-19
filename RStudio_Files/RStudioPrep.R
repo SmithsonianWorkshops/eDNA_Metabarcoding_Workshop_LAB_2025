d8da49f2a15daa8a7c9bf1b6bf56f8ad3fdc7da8
trippster08 <trippster08@outlook.com>
2025-02-19 11:13:12 -0500

    changed name of file

--- RStudio_Files/RStudioPrep.R
+++ RStudio_Files/RStudioPrep.R
@@ -9,7 +9,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
 }
 # Install Dada2. You may get an error telling you to install a different version
 # of Dada2. Change "3.17" to whatever version RStudio tells you.
-BiocManager::install("dada2", version = "3.17")
+BiocManager::install("dada2", version = "3.19")
 # Install Phyloseq
 BiocManager::install("phyloseq")
 # Install DECIPHER
@@ -21,8 +21,6 @@ install.packages("digest")
 install.packages("tidyverse")
 install.packages("seqinr")
 install.packages("ape")
-install.packages("ade4")
-
 
 # Load all the libraries that will be needed for this pipeline. These will have
 # to be reloaded every time you restart RStudio
@@ -34,14 +32,6 @@ library(ape)
 
 
 ## File Housekeeping ===========================================================
-# Set up your working directory. If you created your new project in the
-# directory you want as your working directory, you don't need to do this, and
-# skip to the next RStudio command. If you need to set your working directory,
-# substitute your own path for the one below.
-# Note: If you have spaces or special characters in the path to your working
-# directory, you don't need a character escape (i.e. no \ preceding spaces or
-# special characters).
-setwd ("/Users/USERNAME/Dropbox (Smithsonian)/Projects_Metabarcoding/PROJECTNAME")
 
 # Create all the subdirectories we will use
 # Define the directory names
@@ -52,14 +42,3 @@ dir_names <- c(
 )
 # Create the directories using sapply
 sapply(dir_names, dir.create, recursive = TRUE)
-
-# Find all the read files in the project directory, save their paths, and
-# confirm. Basespace saves the reads in sample-specific folders, using
-# "recursive = TRUE" allows us to find all read files in the working directory
-raw.reads <- list.files(pattern = ".fastq.gz", recursive = TRUE)
-head(raw.reads)
-
-# Copy the read files to the "data/raw" directory, and confirm that they are
-# there.
-file.copy(raw.reads, "data/raw", recursive=TRUE)
-head(list.files("data/raw"))
