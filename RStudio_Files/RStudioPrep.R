aeb16540dd25d59352117e46b592103f96fa9158
trippster08 <trippster08@outlook.com>
2025-02-20 19:10:46 -0500

    added some libraries to install, new folder to create

--- RStudio_Files/RStudioPrep.R
+++ RStudio_Files/RStudioPrep.R
@@ -10,12 +10,11 @@ if (!requireNamespace("BiocManager", quietly = TRUE)){
 # Install Dada2. You may get an error telling you to install a different version
 # of Dada2. Change "3.17" to whatever version RStudio tells you.
 BiocManager::install("dada2", version = "3.19")
-# Install phyloseq
+# Install the rest of the libraries needed through BiocManager.
 BiocManager::install("phyloseq")
-# Install msa
 BiocManager::install("msa")
-# Install DECIPHER
 BiocManager::install("DECIPHER")
+BiocManager::install("rBLAST")
 
 # Install and other libraries you may need (or install through
 # "Install Packages" window). Libraries will only need to be installed once.
@@ -23,15 +22,8 @@ install.packages("digest")
 install.packages("tidyverse")
 install.packages("seqinr")
 install.packages("ape")
-
-# Load all the libraries that will be needed for this pipeline. These will have
-# to be reloaded every time you restart RStudio
-library(dada2)
-library(digest)
-library(tidyverse)
-library(seqinr)
-library(ape)
-
+install.packages("vegan")
+remotes::install_github("fkeck/refdb")
 
 ## File Housekeeping ===========================================================
 
@@ -40,7 +32,8 @@ library(ape)
 dir_names <- c(
   "data/raw",
   "data/working/trimmed_sequences",
-  "data/results"
+  "data/results",
+  "ref"
 )
 # Create the directories using sapply
 sapply(dir_names, dir.create, recursive = TRUE)
