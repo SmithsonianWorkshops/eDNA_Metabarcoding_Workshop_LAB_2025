17c2e2cb730a6836679943252443444cc499031e
trippster08 <trippster08@outlook.com>
2025-02-20 19:10:17 -0500

    started custom database section

---
+++ RStudio_Files/CustomDatabases.R
@@ -0,0 +1,15 @@
+library(refdb)
+library(tidyverse)
+
+refdb_import_BOLD(
+  taxon = "Pteropoda",
+  ids = NULL,
+  bin = NULL,
+  container = NULL,
+  institutions = "Smithsonian Institution",
+  researchers = NULL,
+  geo = NULL,
+  ncbi_taxo = TRUE,
+  full = FALSE
+)
+
