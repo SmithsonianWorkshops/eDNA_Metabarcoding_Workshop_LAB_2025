30dfde5808c6fc4cf1f8d140b19ed673bf60d914
trippster08 <trippster08@outlook.com>
2025-03-01 17:56:13 -0500

    fixed periods, finished section

--- RStudio_Files/8_phyloseq.R
+++ RStudio_Files/8_phyloseq.R
@@ -11,7 +11,6 @@
 
 library(phyloseq)
 library(ape)
-library(msa)
 library(DECIPHER)
 library(tidyverse)
 ## Prepare Components to be Imported Into Phyloseq =============================
@@ -21,27 +20,27 @@ library(tidyverse)
 # Make phyloseq otu_table from the sequence-table (columns of ASV/OTUs, rows of
 # samples). If you want to use a feature-table (columns of samples, rows of
 # ASV/OTUs) instead, use "taxa_are_rows = TRUE"
-OTU_md5 <- otu_table(seqtab_nochim_md5, taxa_are_rows = FALSE)
-View(OTU_md5)
+OTU <- otu_table(seqtab_nochim_md5, taxa_are_rows = FALSE)
+View(OTU)
 ### tax_table ------------------------------------------------------------------
 # Row headings for the tax_table should match the column headings in the
 # otu_table (which in this case are md5 hashes of the ASV's). Also, our current
 # "taxonomy" has a taxonomy table and a bootstrap table, but for the tax_table
 # we need need a taxonomy-only table.
-repseq_nochim_md5 <- repseq.nochim.md5
+
 # Make a new taxonomy-only table, and replace the current rownames (ASVs) with
 # md5 hashes, and convert to a matrix (which is the type of table needed by
 # phyloseq.
-taxonomy_md5 <- taxonomy_rdp_tax %>%
+taxonomy_phyloseq <- taxonomy_rdp_tax %>%
   mutate(RowNames = repseq_nochim_md5) %>%
   column_to_rownames(var = "RowNames") %>%
   select(-ASV) %>%
   as.matrix()
-View(taxonomy_md5)
+View(taxonomy_phyloseq)
 
 # Make phyloseq tax-table from our taxonomy-only table.
-TAX_md5 <- tax_table(taxonomy_md5)
-View(TAX_md5)
+TAX <- tax_table(taxonomy_phyloseq)
+View(TAX)
 
 ### sample_data ----------------------------------------------------------------
 # You should have already imported your metadata in VisualizeResults, but if you
@@ -54,39 +53,41 @@ meta <- read.delim(
 )
 
 # Filter yuur metadata to only include the samples from your dataset.
-meta_dataset1 <- meta %>%
-  filter(Sample_ID %in% rownames(seqtab_nochim))
-
 # Like tax_table and otu_table, sample_data needs rownames as Sample_ID instead
 # of a column. However, unlike the previous commands, sample_data needs a
 # dataframe (unlike otu_table and tax_table, which require a matrix), so we need
-# to convert our first column to a rowname, but that's it.
-meta_matrix <- meta_dataset1 %>%
+# to convert our first column to a rowname, but that's it. We can do all this
+# in one command.
+meta_dataset <- meta %>%
+  filter(Sample_ID %in% rownames(seqtab_nochim)) %>%
   column_to_rownames(var = "Sample_ID")
+View(meta_dataset)
 
 # Look at the metadata file, make sure everything looks okay.
-View(meta_matrix)
+View(meta_dataset)
 
 # Look at data type of all the columns of the table.
-str(meta_matrix)
+str(meta_dataset)
 # Make a phyloseq sample_data file from "metadata"
-SAMPLE <- sample_data(meta_matrix)
+SAMPLE <- sample_data(meta_dataset)
 
 ### refseq ---------------------------------------------------------------------
 # The refseq phyloseq-class item must contain sequences of equal length, which
 # in most cases means it needs to be aligned first. We will align using
 # DECIPHER.
 
-# Create a DNAString from our representative sequences and md5 hashs (like we
-# did in the TaxAssignment section). If you have not made "repseq_nochim_md5"
-# go to the DADA2 section. This is the format for DECIPHER and many other
-# phylogenetic programs in R.
+# We need a DNAString from our representative sequences and md5 hashs. This is
+# the format for DECIPHER and many other phylogenetic programs in R. We should
+# have already made this in the TaxAssignment section. A quick check to see is
+# just to look at it.
+sequences_dna
+# If you don't have it (you get an error), here is the script again.
 sequences_dna <- DNAStringSet(setNames(
   repseq_nochim_md5_asv$ASV,
   repseq_nochim_md5_asv$md5
 ))
 View(sequences_dna)
-
+sequences_dna
 # Align using DECIPHER. DECIPHER "Performs profile-to-profile alignment of
 # multiple unaligned sequences following a guide tree" (from the manual). We do
 # not give a preliminary guide tree, so one is automatically created. For each
@@ -108,8 +109,8 @@ alignment_decipher <- AlignSeqs(
   gapOpening = c(-15, -10),
   gapExtension = c(-3, -2),
   terminalGap = c(-15, -10),
-  iterations = 2,
-  refinements = 3,
+  iterations = 3,
+  refinements = 4,
   processors = NULL,
   useStructures = FALSE
 )
@@ -122,26 +123,9 @@ BrowseSeqs(alignment_decipher)
 
 # Create a reference sequence (refseq) object from the alignment. This contains
 # the ASV sequences, using the md5 hashes as names.
-REFSEQ_md5 <- DNAStringSet(alignment_decipher, use.names = TRUE)
+REFSEQ <- DNAStringSet(alignment_decipher, use.names = TRUE)
 # Look at the refseq object, just to make sure it worked
-head(REFSEQ_md5)
-
-# Align using msa. msa aligns DNA or AA sequences using three possible alignment
-# algorithms (ClustalW, ClustalOega, and MUSCLE). The input format can be a
-# DNAStringSet or direct reading of a fasta file.
-
-alignment_msa <- msa(
-  sequences_dna,
-  method = "ClustalW",
-  gapOpening = 15,
-  gapExtension = 0.2,
-  substitutionMatrix = "clustalw",
-  maxiters = 16,
-  type = "dna",
-  order = "input",
-  verbose = TRUE,
-)
-
+head(REFSEQ)
 
 ### phy_tree -------------------------------------------------------------------
 # We can create a phylogenetic (or at least, a phenetic) tree using the
@@ -149,7 +133,7 @@ alignment_msa <- msa(
 
 # For ape, the aligned sequences must be in binary format (DNAbin, which reduces
 # the size of large datasets), so we first convert the DNAstring alignment.
-alignment_dnabin <- as_DNAbin(alignment_dna)
+alignment_dnabin <- as_DNAbin(alignment_decipher)
 
 # Create pairwise distance matrix in ape. There are many different models to use.
 # Here we are using the Tamura Nei 93 distance measure. Turning the resulting
@@ -160,7 +144,6 @@ pairwise_tn93 <- dist.dna(
   model = "tn93",
   as.matrix = TRUE
 )
-
 # NOTE: If your sequences are highly divergent, pairwise distance will not be
 # calculatable by dist.dna, and you must use another method. I rarely have
 # a distance matrix that does not contain any NaN's (the result for pairs
@@ -173,23 +156,23 @@ pairwise_tn93 <- dist.dna(
 # Check the number of NaNs in dist.dna, and the proportion of all distance. I
 # don't know how many NaNs are too many, but if there are more than a few, I
 # would prefer to be safe and use ml distances.
-length(is.nan(pairwise.tn93))
-length(pairwise.tn93) / length(is.nan(pairwise.tn93))
+length(is.nan(pairwise_tn93))
+length(pairwise_tn93) / length(is.nan(pairwise_tn93))
 
 # Make an improved neighbor-joining tree out of our pairwise distance matrix.
 # ape has other tree-building phenetic methods to use as well, such as nj or
-# upgm. If there are any NaNs in your data, use "bionjs", otherwise us "bionj".
+# upgm. If there are any NaNs in your data, use "bionjs", otherwise use "bionj".
 # If you used a different distance model (ml, K80, F84, etc) replace "tn93"
 # with the model used.
-tree.tn93.bionj <- bionj(pairwise.tn93)
+tree_tn93_bionj <- bionjs(pairwise_tn93)
 
 # Look at the tree. Of course, if there are a lot of ASV's, the tree is pretty
 # much indecipherable, even with lots of editing using ggplot2. We will look at
 # the tree in greater detail from the phyloseq object, below.
-plot(tree.tn93.bionj)
+plot(tree_tn93_bionj)
 
 # Create a phyloseq tree object from our neihbor-joining tree.
-TREE.md5 <- phy_tree(tree.tn93.bionj)
+TREE <- phy_tree(tree_tn93_bionj)
 
 ### phyloseq object ------------------------------------------------------------
 # We use the components created above to create a phyloseq object. For the
@@ -200,13 +183,12 @@ TREE.md5 <- phy_tree(tree.tn93.bionj)
 # suggest you stick with the default format.
 
 physeq <- phyloseq(
-  OTU.md5,
-  TAX.md5,
-  SAMPLE.md5,
-  REFSEQ.md5,
-  TREE.md5
+  OTU,
+  TAX,
+  SAMPLE,
+  REFSEQ,
+  TREE
 )
-
 
 # There are lots of parameters of the phyloseq object you can look at. Part of
 # the reason for looking at these is to make sure the values are what you
@@ -243,6 +225,6 @@ plot_richness(
   measures = c("Shannon", "Fisher"),
   color = "filter_size"
 )
-ord.euclidean <- ordinate(physeq, "MDS", "euclidean")
+ord_euclidean <- ordinate(physeq, "MDS", "euclidean")
 plot_ordination(physeq, ord.euclidean, type = "samples", color = "tank")
 plot_ordination(physeq, ord.euclidean, type = "samples", color = "filter_size")
