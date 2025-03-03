732711268629030ccb22f04bff28247a416b91ca
trippster08 <trippster08@outlook.com>
2025-03-02 20:44:57 -0500

    added raw read counts of samples, fixed formatting, added save Rdata

--- RStudio_Files/3_Cutadapt.R
+++ RStudio_Files/3_Cutadapt.R
@@ -15,8 +15,19 @@ reads_to_trim_R <- reads_to_trim[str_detect(reads_to_trim, "R2_001.fastq.gz")]
 
 # Separate the elements of "reads.to.trim.F" by underscore, and save the first
 # element as "sample.names".
-sample_names <- sapply(strsplit(basename(reads.to_trim_F), "_"), `[`, 1)
-head(sample_names)
+sample_names_raw <- sapply(strsplit(basename(reads.to_trim_F), "_"), `[`, 1)
+head(sample_names_raw)
+
+# Count the number of reads in each sample
+sequence_counts_raw <- sapply(
+  paste0("data/raw/", reads_to_trim_F),
+  function(file) {
+    fastq_data <- readFastq(file)
+    length(fastq_data)
+  }
+)
+names(sequence_counts_raw) <- sample_names_raw
+head(sequence_counts_raw)
 
 # Define the path to your primer definition fasta file if you have more than
 # one potential primer to trim.
@@ -24,46 +35,51 @@ head(sample_names)
 # Again, for the path to the primer files, replace "PRIMERF" or "PRIMERR" with
 # the name of the forward and reverse primer file, respectively.
 
-# THE PATHS SHOWN BELOW ARE EXAMPLES ONLY. PLEASE CHANGE PATH TO YOUR PRIMER FILES.
-path_to_Fprimers <- "eDNA_Metabarcoding_Workshop_LAB_2025-main/primers/COImlIntF.fas"
-path_to_Rprimers <- "eDNA_Metabarcoding_Workshop_LAB_2025-main/primers/jgCOIR.fas"
+# THE PATHS SHOWN BELOW ARE EXAMPLES ONLY. PLEASE CHANGE PATH TO YOUR PRIMER
+# FILES.
+path_to_Fprimers <- "eDNA_Metabarcoding_Workshop_LAB_2025-main/primers/COImlIntF.fas" # nolint: line_length_linter, object_name_linter.
+path_to_Rprimers <- "eDNA_Metabarcoding_Workshop_LAB_2025-main/primers/jgCOIR.fas" # nolint: line_length_linter, object_name_linter.
 
 ## Run Cutadapt ================================================================
 
 # The following for loop runs cutadapt on paired samples, one pair at a time.
-
-for (i in seq_along(sample.names)) {
+# fmt: skip
+for (i in seq_along(sample_names)) {
   system2(
-    cutadapt, args = c(
+    cutadapt_binary,
+    args = c(
       "-e 0.2 --discard-untrimmed --minimum-length 30 --cores=0",
-      "-g", paste0("file:",path_to_Fprimers),
-      "-G", paste0("file:",path_to_Rprimers),
+      "-g", paste0("file:", path_to_Fprimers),
+      "-G", paste0("file:", path_to_Rprimers),
       "-o", paste0(
         "data/working/trimmed_sequences/",
         sample.names[i],
         "_trimmed_R1.fastq.gz"
-      ),
-      "-p", paste0(
+      ), "-p", paste0(
         "data/working/trimmed_sequences/",
         sample.names[i],
         "_trimmed_R2.fastq.gz"
       ),
-      paste0(
-        "data/raw/",
-        reads_to_trim_F[i]
-      ),
-      paste0(
-        "data/raw/",
-        reads_to_trim_R[i]
-      )
-      )
+      paste0("data/raw/", reads_to_trim_F[i]),
+      paste0("data/raw/", reads_to_trim_R[i])
     )
+  )
 }
 
+save(
+  reads_to_trim,
+  reads_to_trim_F,
+  reads_to_trim_R,
+  sample_names_raw,
+  sequence_counts_raw,
+  path_to_Fprimers,
+  path_to_Rprimers,
+  file = "data/working/trim.Rdata"
+)
 # We are including our default parameters for cutadapt. You can change these
 # parameters if you have prefer others.
 
-# -e 0.2 allows an error rate of 0.2 (20% of primer basepairs can me wrong)
+# -e 0.2 allows an error rate of 0.2 (20% of primer basepairs can be wrong)
 
 # --minimum-length 30 removes all reads that are not at least 30 bp. However,
 # as currently implemented in cutadapt, this does not always work correctly,
